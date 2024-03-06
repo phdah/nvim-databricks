06899aa2e681ab54ed6200bd658548df3a76489a
Philip Sjöberg <philip.sjoberg@avinode.com>
2024-03-06 11:15:46 +0100

    Add sleep to notice stagering test print

--- lua/tests/Ptest.py
+++ lua/tests/Ptest.py
@@ -1,5 +1,7 @@
+import time
 for i in range(10):
     if i<1:
         print(f"I haven't had any beers today, that's why the feature isen't done!")
     else:
         print(f"I have had {i} beers already, happy coding!")
+    time.sleep(1)
